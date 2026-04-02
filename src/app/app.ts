3076b170e3a2f5cab2ba1248b38cec0a8b6b98c9
flor-personal <florenciaarnez.ar@gmail.com>
2026-04-02 17:29:21 -0300

    agrego componentes para el proyecto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
+import { ChatItems } from "./components/chat-items/chat-items";
+import { ChatWindow } from "./components/chat-window/chat-window";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, ChatItems, ChatWindow],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/chat-items/chat-items.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-items',
+  imports: [],
+  templateUrl: './chat-items.html',
+  styleUrl: './chat-items.css',
+})
+export class ChatItems {}
