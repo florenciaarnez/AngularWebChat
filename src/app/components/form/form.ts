6b7f8c42f2572d1c7c8ca21a6715245594ad17f5
flor-personal <florenciaarnez.ar@gmail.com>
2026-04-03 19:08:38 -0300

    agrego logica del form con validators, estructura html y estilos

--- src/app/components/form/form.ts
+++ src/app/components/form/form.ts
@@ -1,9 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ChatService } from '../../services/chat';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.html',
   styleUrl: './form.css',
 })
-export class Form {}
+
+export class Form implements OnInit {
+
+  public form!: FormGroup;
+
+  constructor(private fb: FormBuilder,private chatService: ChatService,
+    private router: Router) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      telefono: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      estado: ['', [Validators.required]],
+      avatar: ['', [Validators.required]],
+    });
+  }
+  guardarContacto() {
+    if (this.form.invalid) return;
+
+    const { nombre, telefono, estado, avatar } = this.form.value;
+
+    this.chatService.agregarChat(
+      nombre,
+      Number(telefono),
+      estado,
+      avatar
+    );
+
+    this.form.reset();
+
+    this.router.navigate(['/home']);
+  }
+}
