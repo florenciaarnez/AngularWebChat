76a92b8886d0ca3b736b8cf1e4db398c04be1320
flor-personal <florenciaarnez.ar@gmail.com>
2026-04-03 20:17:05 -0300

    agrego sidebar a home

--- src/app/views/home/home.ts
+++ src/app/views/home/home.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { ChatItems } from "../../components/chatItems/chat-items";
 import { ChatWindow } from "../../components/chatWindow/chat-window";
 import { Header } from "../../components/header/header";
-
+import { Sidebar } from "../../components/sidebar/sidebar";
 @Component({
   selector: 'app-home',
-  imports: [ Header, ChatItems, ChatWindow ],
+  imports: [ ChatWindow, Sidebar ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
