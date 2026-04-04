1562a2243adde93235471a63a91ffc83e313007a
flor-personal <florenciaarnez.ar@gmail.com>
2026-04-04 12:39:54 -0300

    aplico estilos a los chats y estructura html de chat window

--- src/app/components/chatWindow/chat-window.ts
+++ src/app/components/chatWindow/chat-window.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { ChatService } from '../../services/chat';
+import { ReactiveFormsModule } from '@angular/forms';
+import{MessageForm} from '../message-form/message-form';
+import { NgClass } from '@angular/common';
+import { Header } from "../header/header";
 @Component({
   selector: 'app-chat-window',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, MessageForm, NgClass, Header],
   templateUrl: './chat-window.html',
   styleUrl: './chat-window.css',
 })
-export class ChatWindow {}
+export class ChatWindow {
+ public chatService = inject(ChatService);
+
+  nuevoMensaje: string = '';
+}
