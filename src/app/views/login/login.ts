5a87763fb3a321ea0cdf606330d06268ed6017b4
flor-personal <florenciaarnez.ar@gmail.com>
2026-04-03 21:41:57 -0300

    agrego logica para validar el inicio de sesion

--- src/app/views/login/login.ts
+++ src/app/views/login/login.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+function adminValidator(control: any) {
+  return control.value === 'admin' ? null : { notAdmin: true };
+}
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+
+export class Login {
+  public form!: FormGroup;
+  fb = inject(FormBuilder);
+  router = inject(Router);
+ 
+  ngOnInit() {
+    this.form = this.fb.group({
+      contraseña: ['', [Validators.required, adminValidator]]
+    });
+  }
+
+  login() {
+    if (this.form.invalid) return;
+     localStorage.setItem('isLogged', 'true');
+    this.router.navigate(['/home']);
+  }
+
+}
+
