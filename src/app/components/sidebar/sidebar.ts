695abe3fb2da6b144a6a3e72c5d1af627a0a3ac3
flor-personal <florenciaarnez.ar@gmail.com>
2026-04-04 16:27:25 -0300

    agrego boton para añadir chat

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { ChatService } from '../../services/chat';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
