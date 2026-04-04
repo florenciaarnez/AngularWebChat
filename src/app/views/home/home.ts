083b0fa5915cb4cbfb1f0a0b3f0a2a7b5fe4e841
flor-personal <florenciaarnez.ar@gmail.com>
2026-04-04 12:51:30 -0300

    agrego estructura y estilos al home

--- src/app/views/home/home.ts
+++ src/app/views/home/home.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 import { ChatWindow } from "../../components/chatWindow/chat-window";
 import { Sidebar } from "../../components/sidebar/sidebar";
 import { RouterLink } from "@angular/router";
+
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [ChatWindow, Sidebar, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {  
+  }
+
